3e67f8f53781d662cdb202718be584484f93251e
CarlosBitizin <carlospintorossy07@gmail.com>
2022-03-08 16:18:10 -0400

    criando interface

--- lib/jogo.dart
+++ lib/jogo.dart
@@ -10,8 +10,22 @@ class Jogo extends StatefulWidget {
 class _JogoState extends State<Jogo> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("JokenPo"),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Padding(
+            padding: EdgeInsets.only(top: 32, bottom: 16), 
+            child: Text(
+            "Escolha do App:",
+            textAlign: TextAlign.center,
+            ),
+          ), 
+        ],
+      ),
     );
   }
 }
