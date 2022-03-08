cb10c7be74be115162a818a68cfc8de6c8949103
CarlosBitizin <carlospintorossy07@gmail.com>
2022-03-08 16:27:36 -0400

    finalização da interface

--- lib/jogo.dart
+++ lib/jogo.dart
@@ -15,15 +15,39 @@ class _JogoState extends State<Jogo> {
         title: Text("JokenPo"),
       ),
       body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           Padding(
             padding: EdgeInsets.only(top: 32, bottom: 16), 
             child: Text(
             "Escolha do App:",
             textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
             ),
           ), 
+          Image.asset("images/padrao.png"),
+          Padding(
+            padding: EdgeInsets.only(top: 32, bottom: 16), 
+            child: Text(
+            "Escolha uma opção Abaixo:",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Image.asset("images/pedra.png", height: 100),
+              Image.asset("images/papel.png",height: 100),
+              Image.asset("images/tesoura.png",height: 100),
+            ],
+          ),
         ],
       ),
     );
